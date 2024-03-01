ab640213514509a73e82af41a892e561dbcb40c4
itsglyb <geli.robiso@gmail.com>
2024-03-02 01:23:06 +0800

    guest event reg na hindi ko matapos putanginaaaaaaa

--- onboarder/src/app/client/Landing_Page/guest-events-listing/guest-events-listing.component.ts
+++ onboarder/src/app/client/Landing_Page/guest-events-listing/guest-events-listing.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable, catchError, tap } from 'rxjs';
 declare var $: any;
 
 interface OrgEvent {
@@ -27,19 +28,42 @@ interface OrgEvent {
   styleUrls: ['./guest-events-listing.component.css']
 })
 export class GuestEventsListingComponent implements OnInit {
+  eventDesc!: string;
+  poster!: string;
+  memID: string = ""; // Initialize memID property
 
-  constructor(private router: Router, private renderer2: Renderer2, private el: ElementRef, private route: ActivatedRoute, private http: HttpClient) {
+  constructor(private router: Router, private renderer2: Renderer2, private el: ElementRef, private route: ActivatedRoute, private http: HttpClient, private formBuilder: FormBuilder) {
   }
   
   orgEventArray: OrgEvent[] = [];
+  form!: FormGroup;
   orgEvent$: Observable<OrgEvent[]> | undefined;
+  orgID!: string;
+  orgName!:string;
+  eventID!: string;
+  eventTitle!:string;
+  memType: string = 'Guest'; // Declare and initialize memType here
+
   ngOnInit(): void {
     const n = "#nav";
     const no = ".nav-items";
     this.route.params.subscribe(params => {
-      const orgID = params['orgID'];
-      this.getOrgEvent(orgID);
+      this.orgID = params['orgID'];
+      this.eventID = params['_id'];
+      this.orgName = params['orgName'];
+      this.eventTitle = params['eventTitle'];
+      this.memType;
+      this.getOrgEvent(this.orgID);
+      this.getMemberID(); // Call function to get memID
     })
+
+    this.form = this.formBuilder.group({
+      memName: ['', Validators.required],
+      memType: ['', Validators.required],
+      proofofPayment: ['', Validators.required],
+      emailAddress: ['', Validators.required],
+      contactno: ['', Validators.required]
+    });
 
     $(n).click(() => {
       const noElement = this.el.nativeElement.querySelector(no);
@@ -68,28 +92,88 @@ export class GuestEventsListingComponent implements OnInit {
     });
   }
 
-  showModalAfterDelay() {
-    this.showSusbcFormModal(); // Show the modal immediately without delay
-  }
+  showModalAfterDelay(eventID: string, poster: string, eventTitle: string) {
+    this.showSusbcFormModal(eventID, poster, eventTitle);
+}
 
-  showSusbcFormModal() {
+  
+  showSusbcFormModal(eventID: string, poster: string, eventTitle: string) {
     const susbcFormModal = this.el.nativeElement.querySelector('#susbc-form');
+    this.eventID = eventID;
+    this.poster = poster; // Set the eventID property of the component
+    this.eventTitle = eventTitle;
     $(susbcFormModal).modal('show');
-  }
+}
 
+  
   onSubmitForm(e: Event) {
     e.preventDefault();
   
     const subsForm = this.el.nativeElement.querySelector('#subs-form');
     const susbcFormModal = this.el.nativeElement.querySelector('#susbc-form');
     const susbcFormThankModal = this.el.nativeElement.querySelector('#susbc-form-thank');
+    
+    const regForm = this.form.getRawValue();
+    const formData = {
+        orgID: this.orgID,
+        orgName: this.orgName,
+        eventID: this.eventID,
+        memID: this.memID, // Use the memID obtained previously
+        memName: regForm.memName,
+        memType: regForm.memType,
+        proofofPayment: regForm.proofofPayment,
+        emailAddress: regForm.emailAddress,
+        contactno: regForm.contactno
+    };
+    console.log("Response:", formData);
+
+    // Call getMemberID to ensure memID is fetched before registering
+    this.getMemberID().subscribe(() => {
+      // Send the form data to the backend
+      this.registerToEvent(formData);
   
-    // Reset form
-    $(subsForm).trigger('reset');
+      // Reset form
+      $(subsForm).trigger('reset');
+    
+      // Hide form modal and show thank you modal
+      $(susbcFormModal).modal('hide');
+      $(susbcFormThankModal).modal('show');
+    });
+  }
+
+  registerToEvent(formData: any) {
+    this.http.post('http://localhost:5000/api/createRegForm', formData, { withCredentials: true }).subscribe(
+        () => {
+            const subsForm = this.el.nativeElement.querySelector('#subs-form');
+            const susbcFormModal = this.el.nativeElement.querySelector('#susbc-form');
+            const susbcFormThankModal = this.el.nativeElement.querySelector('#susbc-form-thank');
   
-    // Hide form modal and show thank you modal
-    $(susbcFormModal).modal('hide');
-    $(susbcFormThankModal).modal('show');
+            $(subsForm).trigger('reset');
+  
+            // Hide form modal and show thank you modal
+            $(susbcFormModal).modal('hide');
+            $(susbcFormThankModal).modal('show');
+        },
+        (err) => {
+            console.error('Error registering to the event:', err);
+            // Handle the error here
+        }
+    );
+}
+
+  getMemberID(): Observable<any> {
+    // Make HTTP request to fetch memID
+    return this.http.get<any>('http://localhost:5000/api/member',  { withCredentials: true })
+      .pipe(
+        tap((response) => {
+          this.memID = "Guest"; // Assuming memID is available in the response
+          console.log('Member ID:', this.memID);
+        }),
+        catchError((error) => {
+          console.error('Error fetching member ID:', error);
+          throw error;
+        })
+      );
   }
 
   closeModal() {
@@ -100,18 +184,4 @@ export class GuestEventsListingComponent implements OnInit {
   redirectToLogin() {
     this.router.navigate(['/auth-login']);
   }
-  // constructor(private el: ElementRef) {}
-
-  // ngOnInit() {
-  //   $(document).ready(() => {
-  //     $('#openBtn').click((event: Event) => {
-  //       event.preventDefault();
-  //       $('#myModal').modal('show');
-  //     });
-  //   });
-  // }
-
-  // closeModal() {
-  //   $('#myModal').modal('hide');
-  // }
 }
