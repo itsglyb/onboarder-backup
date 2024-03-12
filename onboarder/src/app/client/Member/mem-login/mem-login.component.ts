c2b5d478d6025794e47090ff2807f5d0c7b62006
JC Magno <106789015+Badbaddie28@users.noreply.github.com>
2024-03-12 20:10:27 +0800

    MemCategory ID updated

--- onboarder/src/app/client/Member/mem-login/mem-login.component.ts
+++ onboarder/src/app/client/Member/mem-login/mem-login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import * as $ from 'jquery';
@@ -9,29 +9,30 @@ interface ServerResponse {
   userType: string;
   // You can add other properties received from the server if needed
 }
+
 @Component({
   selector: 'app-mem-login',
   templateUrl: './mem-login.component.html',
   styleUrls: ['./mem-login.component.css']
 })
-export class MemLoginComponent implements OnInit{
+export class MemLoginComponent implements OnInit {
   form!: FormGroup;
-  userType!:string;
+  submitted: boolean = false;
+
   constructor(
     private formBuilder: FormBuilder,
-    private http:HttpClient,
+    private http: HttpClient,
     private router: Router,
-    
-    ) {}
+  ) {}
 
   ngOnInit(): void {
+    // validations
     this.form = this.formBuilder.group({
-      email:"",
-      password:""
-
-    })
-
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
+
   togglePassword(): void {
     const passwordInput = document.getElementById('form3Example4') as HTMLInputElement;
     const passwordIcon = document.querySelector('.toggle-password') as HTMLElement;
@@ -47,66 +48,36 @@ export class MemLoginComponent implements OnInit{
     }
   }
 
-  ValidateEmail = (email: any) => {
- 
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  
-    if (email.match(validRegex)) {  
-  
-      return true;
-  
-    } else {
-  
-      return false;
-  
+  submit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
     }
 
-  }
-    submit(): void{
-    
-  
-      let member = this.form.getRawValue()
-      console.log(member)
-      if( member.email == "" || member.password == ""){
-        Swal.fire("Error", "Please fill up all the required fields.", "error")
+    const member = this.form.value;
+
+    this.http.post('http://localhost:5000/api/login', member, { withCredentials: true }).subscribe(
+      (res: any) => {
+        this.http.get('http://localhost:5000/api/current', { withCredentials: true }).subscribe(
+          (userData: any) => {
+            if (userData.userType === 'member') {
+              this.router.navigate(['/member-profile']);
+            } else if (userData.userType === 'organization') {
+              this.router.navigate(['/org-profile']);
+            } else if (userData.userType === 'admin') {
+              this.router.navigate(['/admin-users']);
+            } else {
+              // Handle other user types or navigate to appropriate pages
+            }
+          },
+          (err) => {
+            Swal.fire('Error', err.error.message, 'error');
+          }
+        );
+      },
+      (err) => {
+        Swal.fire('Error', err.error.message, 'error');
       }
-    else if(!this.ValidateEmail(member.email)){
-   
-      Swal.fire('Error', 'Please enter a valid email address', 'error');
-  
-    } else {
-  
-    this.http
-      .post('http://localhost:5000/api/login', member, {
-        withCredentials: true,
-        
-      })
-      .subscribe(
-        (res: any) => {
-          this.http.get('http://localhost:5000/api/current', { withCredentials: true })
-            .subscribe(
-              (userData: any) => {
-                if (userData.userType === 'member') {
-                  this.router.navigate(['/member-profile']);
-                } else if (userData.userType === 'organization') {
-                  this.router.navigate(['/org-profile']);
-                } else if (userData.userType === 'admin') {
-                  this.router.navigate(['/admin-users']);
-                }else {
-                  // Handle other user types or navigate to appropriate pages
-                }
-              },
-              (err) => {
-                Swal.fire('Error', err.error.message, 'error');
-              }
-            );
-        },
-        (err) => {
-          Swal.fire('Error', err.error.message, 'error');
-        }
-  
-      );
-  
+    );
   }
-}
 }
