d62949385d3872bcc991639e7c911b94750e0a07
JC Magno <106789015+Badbaddie28@users.noreply.github.com>
2024-03-12 21:28:08 +0800

    Admin-OrgApproval Corrected

--- onboarder/src/assets/js/admin-org.js
+++ onboarder/src/assets/js/admin-org.js
@@ -5,13 +5,9 @@ $(document).on('click', '.btn-danger', function() {
 
 // Event handler for the "Yes" button click in the delete modal
 
-// Event handler for the "Edit" button click
-$(document).on('click', '.btn-primary', function() {
-    $('#editOrgModal').modal('show');
-});
 
 
 // Event handler for the "View" button click
-$(document).on('click', '.btn-success', function() {
+$(document).on('click', '.btn-primary', function() {
     $('#viewOrgModal').modal('show');
 });
