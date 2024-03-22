a8cafb98d1487f9155bdfd68a607dafd296c08d1
itsglyb <geli.robiso@gmail.com>
2024-03-22 14:45:58 +0800

    k

--- server/index.js
+++ server/index.js
@@ -26,10 +26,10 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use("/api", routes)
 
-app.use(express.static(path.join(__dirname, '/dist/onboarder')));
+app.use(express.static(path.join(__dirname, '../dist/onboarder')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/onboarder/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/onboarder/index.html'));
 });
 
 
